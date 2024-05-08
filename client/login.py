253aef8bdd75e01799a543c8a3339eeda3500631
David Solomon <david@mac-mini.mynetworksettings.com>
2024-05-07 19:40:37 -0700

    Separate menu into its own function

--- client/login.py
+++ client/login.py
@@ -3,11 +3,8 @@
 import sys
 from account import Account
 
-
-def main():
+def main_menu():
     user_account = Account()
-
-    print("Welcome to Neverquest!")
     print("1. Login")
     print("2. Create Account")
     print("3. Exit")
@@ -19,12 +16,19 @@ def main():
     if choice == "3":
         print("Goodbye!")
         sys.exit()
+    else: 
+        print(f"\nSelect a valid option.")
+        main_menu()
 
 def user_login(user_account):
     user_account.login()
 
 def user_create_account(user_account):
     user_account.create_account()
+
+def main():
+    print("Welcome to Neverquest!")
+    main_menu()
 
 if __name__ == "__main__":
     main()
